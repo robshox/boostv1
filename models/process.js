84be5913e47ebff1fe5f78770bc3b6837fdeea16
Robert O'Shaughnessy <rob@robshox.com>
2017-07-12 10:25:35 +0000

    Changed process model to create collection

--- models/process.js
+++ models/process.js
@@ -1,8 +1,8 @@
 
 var mongoose = require('mongoose');
 
-var folderSchema = new mongoose.Schema({
+var processchema = new mongoose.Schema({
   name: String,
 });
 
-module.exports = mongoose.model('Process', folderSchema);
+module.exports = mongoose.model('Process', processSchema);
